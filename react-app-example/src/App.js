a426174bc42821966f298a3b378d5dc4d49391a7
amandayong <amandyong@gmail.com>
2022-07-07 15:26:12 +0800

    codes for routing #5

--- react-app-example/src/App.js
+++ react-app-example/src/App.js
@@ -2,6 +2,21 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 
+import {
+  Route,
+  useParams,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
+
+// import screens
+import NavBar from './components/NavBar';
+import HomeScreen from './screens/HomeScreen';
+import ProductsScreen from './screens/ProductsScreen';
+import NoScreen from './screens/404Screen';
+import FooterBar from './components/FooterBar';
+import SingleProductScreen from './screens/SingleProductScreen';
+
 class App extends React.Component
 {
     constructor() {
@@ -13,7 +28,17 @@ class App extends React.Component
         return (
             <div>
               <NavBar />
-                <h1>Hello React!</h1>
+                {/* Router */}
+              <BrowserRouter>
+                  <Routes>
+                    <Route exact path="/" element={<HomeScreen/>}/>
+                    <Route path="/products" element={<ProductsScreen/>}/>
+                    <Route path="/products/:productId" element={<SingleProductScreen/>}/>}/>
+                    <Route path="*" element={<NoScreen/>}/>
+                  </Routes>
+                </BrowserRouter>
+
+            <FooterBar />
             </div>
         );
     }
